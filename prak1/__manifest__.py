9bf4fa7dc2567ab7e644da9ca114b5f333b73eec
NaufalAlfi <naufalalfi100@gmail.com>
2023-02-20 11:42:22 +0700

    addon

---
+++ prak1/__manifest__.py
@@ -0,0 +1,21 @@
+{
+"name" : "New Addons",
+"version" : "1.0",
+"author" : "Naufal Fauzan Alfi",
+"website"       : "https://www.detik.com/",
+"category"      : "New Module",
+"summaryy"      : "Ini adalah addons baru",
+"description"   : """Latihan membuat addons baru di odoo 1""",
+"depends"       : [
+    "base",   
+],
+"data": [],
+"demo"          :[],
+"test"          :[],
+"image"         :[],
+"qweb"          :[],
+"css"           :[],
+"application"   :True,
+"installable"   :True,
+"auto_install"  :False,
+}   
